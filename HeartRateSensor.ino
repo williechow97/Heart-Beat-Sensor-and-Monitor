302e5d3f3cd91d1fe348623ed251242b9b168216
williechow97 <willie.chow01@gmail.com>
2021-01-22 00:11:01 -0800

    Add files via upload
    Arduino Code for IR transmitter and receiver for PPG heart beat detection

---
+++ HeartRateSensor.ino
@@ -0,0 +1,16 @@
+//Constants
+const int irReceiver=A0; //IR Receiver is at Arduino analog pin A0
+//Variables 
+int value;              //Store value of IR Receiver
+int i=0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(irReceiver, INPUT);  //Set irReceiver - A0 pin as an input 
+}
+
+void loop() {
+  value=analogRead(irReceiver);
+  Serial.println(value);
+  //delay(50); //delay Arduino in milliseconds
+}
