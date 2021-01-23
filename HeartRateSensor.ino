dc78f8a495491394db883a79b0fb9f62e6a0c739
williechow97 <willie.chow01@gmail.com>
2021-01-23 13:52:24 -0800

    Update HeartRateSensor.ino
    added in sampling_interval and other parameters

--- HeartRateSensor.ino
+++ HeartRateSensor.ino
@@ -1,8 +1,10 @@
 //Constants
 const int irReceiver=A0; //IR Receiver is at Arduino analog pin A0
 //Variables 
-int value;              //Store value of IR Receiver
-int i=0;
+int value = 0;              //Store value of IR Receiver
+unsigned long currentMicros;
+unsigned long previousMicros=0;
+unsigned long sampling_interval=40000;
 
 void setup() {
   Serial.begin(9600);
@@ -10,7 +12,11 @@ void setup() {
 }
 
 void loop() {
-  value=analogRead(irReceiver);
-  Serial.println(value);
+  currentMicros = micros();
+  if(currentMicros-previousMicros >= sampling_interval){
+    value=analogRead(irReceiver);  
+    Serial.println(value);
+    previousMicros = currentMicros;
+  }
   //delay(50); //delay Arduino in milliseconds
 }
